bf628bcb2a318f86527a5f45645412ead19d09dd
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-10 18:36:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2 
+    return array.join(" and ")
+  else array.length >= 3
+    a = array[-1]
+    a.prepend("and ")
+    return array.join(", ")
+  end
 end
